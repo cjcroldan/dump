778081d64275492d2973624c9f8935548710d476
cjcroldan <92191056+cjcroldan@users.noreply.github.com>
2022-09-23 12:13:02 +0800

    Add files via upload

---
+++ SingerClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TAU
+ */
+public class SingerClass {
+    String name;
+    int noOfPerformances;
+    double earnings;
+    
+    
+    public void performForAudience () {
+        noOfPerformances += 1;
+        earnings += 100;
+    }
+  
+    public void changeFavSong() {
+        earnings -= 2;
+    }
+}
---
+++ songClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TAU
+ */
+public class songClass {
+    // Make another class called Song with at least one constructor. Determine the fields and methods that would belong to such a class and create another two instances.
+    
+}
