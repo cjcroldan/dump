240cdd3962ae8e71c28e925d96e3cbf4fae9cf2a
cjcroldan <92191056+cjcroldan@users.noreply.github.com>
2022-09-23 11:39:50 +0800

    Add files via upload

---
+++ JavaProject1.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject1;
+    import java.util.Scanner;
+/**
+ *
+ * @author TAU
+ */
+public class JavaProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DATA TYPES
+        int age = 25; // whole number 
+        double money = 12000.05; // has decimal
+        boolean isWorking = true; // condition must be lower case
+        char section = 'D'; // uses single quotes
+        String name = "Mark"; // uses double quotes mostly
+        // byte, short, long, float
+        
+        //OPERATORS
+        // arithmetic operators
+        // comparison operators
+        // logical operators
+        // dividing integers would result in an integer (int)
+        
+        //CONTROL FLOW STATEMENTS
+        // if-else statements
+        // switch case statement (don't forget to add a break statement)
+        // loops: while, for, do-while
+        // boolean values don't need = true/false
+        // add a not(!) to check if a statement is not true
+        
+        //I/O
+        System.out.print("yass");
+            /* Scanner sc = new Scanner(System in);
+            System.out.print("What's your name?");
+            String name = sc.nextLine();
+            System.out.print("Hello, %s.", name);
+            int age = Integer.parseInt(sc.nextLine); */
+            
+        
+        // TODO code application logic here
+    }
+    
+}
